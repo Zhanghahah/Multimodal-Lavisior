414a408656bc8a2bc2b3be10903162e16a76fbe3
Zhanghahah <572709035@qq.com>
2023-11-10 12:53:16 +0000

    add inference

---
+++ inference.sh
@@ -0,0 +1,5 @@
+python inference.py --cfg-path eval_configs/drugchat_eval.yaml --gpu-id 1 --in_file /home/zhangyu/data/uspto/chem_uspto_instruction_test_v2.json --out_file /home/zhangyu/data/uspto/uspto_forward_prediction_results.json
+
+#python inference.py --cfg-path eval_configs/drugchat_graph_image_infer.yaml --gpu-id 0 --in_file data/ChEMBL_QA_test.json --out_file eval_results/ChEMBL_QA_test_graph_img_trained_ChEMBL.json
+
+
